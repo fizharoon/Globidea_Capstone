1eddfc9e466b112007195f2c86e62e9ca99905f3
fizharoon <fizharoon@gmail.com>
2023-03-12 22:12:03 -0700

    Runs react through django

--- Globidea/main/urls.py
+++ Globidea/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 from . import views #import views from current directory
-
+# urlpatterns = [
+#     path("",views.index, name="index"),
+# ]
 urlpatterns = [
-    path("",views.index, name="index"),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
