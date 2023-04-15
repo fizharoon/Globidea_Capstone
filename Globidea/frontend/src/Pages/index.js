8b2db048aeeb89aa6a5cea2bdb2ebd6ba1664dd2
dodoli33 <124644759+dodoli33@users.noreply.github.com>
2023-04-14 18:31:38 -0700

    Add image hover effect

--- Globidea/frontend/src/Pages/index.js
+++ Globidea/frontend/src/Pages/index.js
@@ -4,28 +4,42 @@ import timelineImg2 from "../assets/timeline-img-2.png";
 import timelineImg3 from "../assets/timeline-img-3.png";
 import timelineImg4 from "../assets/timeline-img-4.png";
 import timelineImg5 from "../assets/timeline-img-5.png";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to our website!</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-
-
-        <Link to="/phase1" style={{ transform: "skewX(-8deg)", perspective: "1000px", transformStyle: "preserve-3d", margin: "0 10px" }}title="Planning to Attend">
-          <img src={timelineImg1} alt="timeline-img-1" style={{ width: "200px", height: "300px", transform: "skewX(8deg) rotateY(20deg)" }} />
+      <div className="timeline-container">
+        <Link to="/phase1" className="timeline-item" title="Planning to Attend">
+          <div className="timeline-item-content">
+            <img src={timelineImg1} alt="timeline-img-1" />
+            <h2>Planning to Attend</h2>
+          </div>
         </Link>
-        <Link to="/phase2" style={{ transform: "skewX(-8deg)", perspective: "1000px", transformStyle: "preserve-3d", margin: "0 10px" }}title="Getting Started">
-          <img src={timelineImg2} alt="timeline-img-2" style={{ width: "200px", height: "300px", transform: "skewX(8deg) rotateY(-20deg)" }} />
+        <Link to="/phase2" className="timeline-item" title="Getting Started">
+          <div className="timeline-item-content">
+            <img src={timelineImg2} alt="timeline-img-2" />
+            <h2>Getting Started</h2>
+          </div>
         </Link>
-        <Link to="/phase3" style={{ transform: "skewX(-8deg)", perspective: "1000px", transformStyle: "preserve-3d", margin: "0 10px" }}title="Making Progress">
-          <img src={timelineImg3} alt="timeline-img-3" style={{ width: "200px", height: "300px", transform: "skewX(8deg) rotateY(20deg)" }} />
+        <Link to="/phase3" className="timeline-item" title="Making Progress">
+          <div className="timeline-item-content">
+            <img src={timelineImg3} alt="timeline-img-3" />
+            <h2>Making Progress</h2>
+          </div>
         </Link>
-        <Link to="/phase4" style={{ transform: "skewX(-8deg)", perspective: "1000px", transformStyle: "preserve-3d", margin: "0 10px" }}title="Prepare to graduate">
-          <img src={timelineImg4} alt="timeline-img-4" style={{ width: "200px", height: "300px", transform: "skewX(8deg) rotateY(-20deg)" }} />
+        <Link to="/phase4" className="timeline-item" title="Prepare to graduate">
+          <div className="timeline-item-content">
+            <img src={timelineImg4} alt="timeline-img-4" />
+            <h2>Prepare to graduate</h2>
+          </div>
         </Link>
-        <Link to="/phase5" style={{ transform: "skewX(-8deg)", perspective: "1000px", transformStyle: "preserve-3d", margin: "0 10px" }}title="Moving Forward">
-          <img src={timelineImg5} alt="timeline-img-5" style={{ width: "200px", height: "300px", transform: "skewX(8deg) rotateY(20deg)" }} />
+        <Link to="/phase5" className="timeline-item" title="Moving Forward">
+          <div className="timeline-item-content">
+            <img src={timelineImg5} alt="timeline-img-5" />
+            <h2>Moving Forward</h2>
+          </div>
         </Link>
       </div>
       <button>
